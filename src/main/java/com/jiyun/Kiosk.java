b8277e1bb9bb44bb4e7a215cc4f2b27c2f783042
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-03-10 14:24:31 +0900

    feat: 예외 처리 추가

--- src/main/java/com/jiyun/Kiosk.java
+++ src/main/java/com/jiyun/Kiosk.java
@@ -25,6 +25,8 @@ public class Kiosk {
                     System.out.println("선택한 메뉴: " + menu.getMenuItems().get(inputNum - 1));
                 } catch (NumberFormatException e) {
                     System.out.println("숫자를 입력해 주세요.");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("유효하지 않은 입력입니다.");
                 }
             }
         }
