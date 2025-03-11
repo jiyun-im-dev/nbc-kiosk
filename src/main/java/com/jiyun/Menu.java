2f4898822525c694bf34af21c677f9d506cdf27e
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-03-11 17:06:24 +0900

    refactor: Menu 클래스를 싱글톤으로 변경

--- src/main/java/com/jiyun/Menu.java
+++ src/main/java/com/jiyun/Menu.java
@@ -11,8 +11,12 @@ public class Menu {
 
     private final List<MenuItem> menuItems = new ArrayList<>();
 
+    @Getter
+    private static final Menu menu = new Menu(); // 싱글톤
+
     // 기본 생성자에서 메뉴를 초기화
-    public Menu() {
+    // 싱글톤은 객체를 단 하나만 생성해서 공유해야 하므로 생성자를 private 으로 막음
+    private Menu() {
         menuItems.add(new MenuItem(Category.BURGER, "ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
         menuItems.add(new MenuItem(Category.BURGER, "SmokeShack", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
         menuItems.add(new MenuItem(Category.BURGER, "Cheeseburger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
